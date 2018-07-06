304094d265c82955e4fe371c64c25d8ff20abcee
dimitraz <dzuccare@redhat.com>
2018-07-06 10:39:31 +0100

    fix: remove unused util function

--- pkg/apis/androidsdk/v1/utils.go
+++ pkg/apis/androidsdk/v1/utils.go
@@ -6,27 +6,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
-func IsValidSdkConfig(configMap *corev1.ConfigMap) bool {
-	labels := configMap.Labels
-	expectedAppLabel := "android-sdk-persistent"
-	expectedCtxLabel := "android-sdk-config"
-	appLabel, ok := labels["app"]
-	if !ok {
-		return false
-	}
-
-	ctxLabel, ok := labels["context"]
-	if !ok {
-		return false
-	}
-
-	if expectedAppLabel == appLabel && expectedCtxLabel == ctxLabel {
-		return true
-	}
-
-	return false
-}
-
 func GetConfigData(configMap *corev1.ConfigMap) (string, error) {
 	data, ok := configMap.Data["packages"]
 
